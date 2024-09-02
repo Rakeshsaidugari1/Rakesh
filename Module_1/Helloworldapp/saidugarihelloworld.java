63a37f89a2e6e71bf67ce3fb1e9aeb2cfd7340b6
Rakeshsaidugari1 <rsaidugari@my365.bellevue.edu>
2024-09-01 20:10:15 -0500

    added a module_1 file.

---
+++ Module_1/Helloworldapp/saidugarihelloworld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String firstName = "Rakesh";
+        String lastName = "Saidugari";
+        System.out.printf("Hello World from %s %s.%n", firstName, lastName);
+    }
+}
